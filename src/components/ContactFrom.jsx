847d9411a9a0c966e1a24ffa5d4d27c345146540
keshavnagar <keshavnagar887@gmail.com>
2025-12-04 17:17:52 +0530

    complete the contact form with saparate state

---
+++ src/components/ContactFrom.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const ContactForm = () => {
+  const [name, setName] = useState("Keshav");
+  const [email, setEmail] = useState("keshavnagar887@gmail.com");
+  const [message, setMessage] = useState("hello lets connect");
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "message":
+        setMessage(value);
+        break;
+      default:
+        console.log("wrong choice");
+    }
+  };
+  const handleForm = (e) => {
+    e.preventDefault();
+    const user = {
+      name,
+      email,
+      message,
+    };
+    console.log(user);
+  };
+  return (
+    <>
+      <form onSubmit={handleForm}>
+        <label>
+          Name:{" "}
+          <input type="text" value={name} name="name" onChange={handleInput} />
+        </label>
+        <label>
+          Email:{" "}
+          <input
+            type="email"
+            value={email}
+            name="email"
+            onChange={handleInput}
+          />
+        </label>
+        <label>
+          Message:{" "}
+          <input
+            type="text"
+            value={message}
+            name="message"
+            onChange={handleInput}
+          />
+        </label>
+      </form>
+    </>
+  );
+};
+export default ContactForm;
